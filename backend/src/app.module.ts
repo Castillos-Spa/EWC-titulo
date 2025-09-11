a36a320cc4f063dea53827dc970552761f25e377
Bacze12 <brunocc.0205@gmail.com>
2025-09-11 12:50:06 -0300

    modulo ticket pero falta logica

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,26 +6,26 @@ import { UsersModule } from './users/users.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { ConfigModule } from '@nestjs/config';
-import { TicketsModule } from './tickets/tickets.module';
 import { TallerModule } from './taller/taller.module';
 import { VehiculoModule } from './vehiculo/vehiculo.module';
 import { OrdenTrabajoModule } from './orden-trabajo/orden-trabajo.module';
 import { QaModule } from './qa/qa.module';
 import { SolicitudCompraModule } from './solicitud-compra/solicitud-compra.module';
 import { RepuestoModule } from './repuesto/repuesto.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
-    TicketsModule,
     TallerModule,
     VehiculoModule,
     OrdenTrabajoModule,
     QaModule,
     SolicitudCompraModule,
     RepuestoModule,
+    TicketModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
