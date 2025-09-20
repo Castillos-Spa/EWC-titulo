63a7a0f8855ba491a0bcbb0be78ad3a4f2479539
Bacze12 <brunocc.0205@gmail.com>
2025-09-20 13:57:33 -0300

    agrega componente UserProfile y actualiza navegación para incluir perfil de usuario

--- backend/src/taller/taller.module.ts
+++ backend/src/taller/taller.module.ts
@@ -5,10 +5,18 @@ import { PrismaModule } from 'prisma/prisma.module';
 import { QaModule } from '@/qa/qa.module';
 import { QaService } from '@/qa/qa.service';
 import { OrdenTrabajoModule } from '@/orden-trabajo/orden-trabajo.module';
+import { VehiculoModule } from '../vehiculo/vehiculo.module';
+import { SolicitudCompraModule } from '../solicitud-compra/solicitud-compra.module';
+import { RepuestoModule } from '../repuesto/repuesto.module';
 
 @Module({
-  imports: [PrismaModule, QaModule, OrdenTrabajoModule],
+  imports: [PrismaModule, QaModule, OrdenTrabajoModule, VehiculoModule, SolicitudCompraModule, RepuestoModule],
   controllers: [TallerController],
   providers: [TallerService, { provide: 'IQaService', useClass: QaService }],
+  exports: [
+    TallerService, // Export TallerService if it needs to be used by other modules
+    OrdenTrabajoModule, // Export OrdenTrabajoModule if its services are used elsewhere
+    QaModule, // Export QaModule if its services are used elsewhere
+  ],
 })
 export class TallerModule {}
