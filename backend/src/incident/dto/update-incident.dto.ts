30e9af91532f584597c63990351a9b069d3d4fc1
Bacze12 <brunocc.0205@gmail.com>
2025-10-07 20:28:48 -0300

    feat: enhance incident update functionality with status management and quick resolve option

--- backend/src/incident/dto/update-incident.dto.ts
+++ backend/src/incident/dto/update-incident.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateIncidentDto } from './create-incident.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateIncidentDto extends PartialType(CreateIncidentDto) {}
+export class UpdateIncidentDto extends PartialType(CreateIncidentDto) {
+  @IsOptional()
+  @IsString()
+  Status?: 'REPORTED' | 'ACKNOWLEDGED' | 'IN_PROGRESS' | 'RESOLVED';
+}
